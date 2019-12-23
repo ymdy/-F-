2e074e1fd78b3a4f83bbebf735daf3051a31206c
YoshiyukiSaito123 <yosisi@ca2.so-net.ne.jp>
2019-12-23 06:47:34 +0000

    天気情報の絞り込みとエラー処理を追記しました。また、mainの方で地域コードをタプル化しています。

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Weather import Weather
 if __name__ == '__main__':
+    locate = ('130010', '140010', '120010') #東京、神奈川、千葉
+    selectday = ('今日', '明日', '明後日')
     weather = Weather()
-    test = weather.returnweather('今日', '130010')
-    print(test[0],test[1])
+    test = weather.returnweather(selectday[0], locate[0])
+    print(test[0] + 'の' + test[1] + 'の天気は' + test[2] + 'です。')
